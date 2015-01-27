e3222f82e747b85298b3fc3dbeaf38c9fbd7ef14
Mark <Mark@10.30.18.113>
2015-01-27 12:28:56 -0600

    Modified Startp class added hello world

---
+++ src/pkgclass/pkg3/abstractions/EnergyDrink.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgclass.pkg3.abstractions;
+
+/**
+ *
+ * @author Mark
+ */
+public class EnergyDrink implements Drink{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ src/pkgclass/pkg3/abstractions/SmartPhone.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgclass.pkg3.abstractions;
+
+/**
+ *
+ * @author Mark
+ */
+public class  SmartPhone implements Phone {
+    private String brand;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    
+    
+}
